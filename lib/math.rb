8c89740bae44049a4f4e3556d0a722c1d015dcee
arhamm ghazali <arhamm_ghazali93@hotmail.com>
2020-09-08 12:36:18 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,27 +1,21 @@
 def addition(num1, num2)
-puts num1 + num2 
-addition(5,4)
-return(9)
+  num1 + num2 
 end
 
 def subtraction(num1, num2)
-puts num1 - num2
-subtraction (10-5)
+  num1 - num2
 end
 
 def division(num1, num2)
-puts = num1 - num2 
-division (50/2)
+  num1 - num2 
 end
 
 def multiplication(num1, num2)
-puts num1 * num2
-multiplication (4**30)
+ num1 * num2
 end
 
 def modulo(num1, num2)
-puts num1 % num2
-modulo (34%5)
+  num1 % num2
 end
 
 def square_root(num)
